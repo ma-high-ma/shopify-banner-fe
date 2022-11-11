b750fcb7f62bb3fd06c68a63ebbe04973cd28244
ma-high-ma <mahima@pushowl.com>
2022-11-12 02:12:47 +0530

    added todo

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -6,6 +6,7 @@ window.website_id = foo
 console.log('outermost part')
 
 function changeButtonStatus(status) {
+    // Todo: Store status in variables
     alert('You clicked me')
     return fetch('https://mahima-be.app.vtxhub.com/banners/?website_id='+window.website_id+'&status='+status, {
         method: 'POST',
@@ -21,6 +22,7 @@ const Home = () => {
   return (
       <div>
       <h1>Main page</h1>
+         {/*Todo: Add toggle to enable or disable banner*/}
       <button onClick={() => changeButtonStatus('enabled')}>Enable Banner</button>
       <button onClick={() => changeButtonStatus('disabled')}>Disable Banner</button>
       </div>
